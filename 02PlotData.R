70b6c9f71dd202c4c2c84da1ae1caf0a323a1612
Ryan Brideau <ryan.brideau@gmail.com>
2014-07-28 19:29:55 -0300

    Did first rough plot of all rows

--- 02PlotData.R
+++ 02PlotData.R
@@ -2,9 +2,21 @@ plot.data <- read.csv("Levy2014Sums100.csv", header=TRUE, stringsAsFactors=FALSE
 
 max <- max(plot.data)
 
-plot(0, 0, xlim=c(0, length(plot.data[[1]])), ylim=c(0,1000), type="n", las=1, xlab="Longitude", ylab="Latitude", bty="n")
+plot(0, 0, xlim=c(0, length(plot.data[[1]])), ylim=c(0,1300), type="n", las=1, xlab=NA, ylab=NA, bty="n")
+
+plottingHeight <- 1000
+scaleFactor <- 400
 
 for (i in 1:length(plot.data)) {
-  lines(1:length(plot.data[[1]]), plot.data[[i]]/max*400, col="#000000")
-  print(i)
+  # lines(1:length(plot.data[[1]]), plot.data[[i]]/max*400, col="#000000")
+  
+  row <- as.vector(plot.data[[i]]/max*scaleFactor + plottingHeight)
+  
+  xes <- c(0, 0:(length(plot.data[[1]]) - 1), length(plot.data[[1]]))
+  ys <- c(plottingHeight, row, plottingHeight)
+  
+  polygon(x = xes, y = ys, col = "#EDEDED50")
+  
+  plottingHeight <- plottingHeight - 10
 }
+
